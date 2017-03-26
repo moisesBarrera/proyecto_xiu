9c49f4220743cb11394ca8e434ebed225916576c
25ls <ernesto25ls@gmail.com>
2017-03-26 17:09:50 -0500

    Checa el registro
    Checa en donde la cague

--- SGG/app/src/main/java/com/example/eisra/sgg/DatosEscolares.java
+++ SGG/app/src/main/java/com/example/eisra/sgg/DatosEscolares.java
@@ -1,13 +1,191 @@
 package com.example.eisra.sgg;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class DatosEscolares extends AppCompatActivity {
 
+    private TextView nombre,cct,sector,zona,direccion;
+    private Spinner turno;
+    private Button boton;
+public String ip="192.168.1.68";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos_escolares);
+       // new ConsultarDatos().execute("http://"+ip+"/android/consulta.php?codigo=" + codigo.getText().toString());
+
+
+        nombre=(TextView) findViewById(R.id.textView3);
+        cct=(TextView) findViewById(R.id.textView4);
+        sector=(TextView) findViewById(R.id.textView7);
+        zona=(TextView) findViewById(R.id.textView8);
+        direccion=(TextView) findViewById(R.id.textView9);
+        turno=(Spinner) findViewById(R.id.spTurno);
+        boton =(Button) findViewById(R.id.button);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  if(nombre.getText().toString().trim()==null && cct.getText().toString().trim()==null & zona.getText().toString().trim()==null && direccion.getText().toString().trim()==null)
+
+              //  new CargarDatos().execute("http://"+ip+"/android/registro.php?nombre=" + nombre.getText().toString()+"&direccion="+direccion.getText().toString()+"&telefono="+telefono.getText().toString());
+
+            }
+        });
+
+    }
+
+
+
+    private class CargarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            Toast.makeText(getApplicationContext(), "Se almacenaron los datos correctamente", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private class EditarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            Toast.makeText(getApplicationContext(), "Se editaron los datos correctamente", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private class EliminarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            Toast.makeText(getApplicationContext(), "Se eliminaron los datos correctamente", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private class ConsultarDatos extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            JSONArray ja = null;
+            try {
+                ja = new JSONArray(result);
+                nombre.setText(ja.getString(1));
+                direccion.setText(ja.getString(2));
+                cct.setText(ja.getString(3));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
+    private String downloadUrl(String myurl) throws IOException {
+        Log.i("URL",""+myurl);
+        myurl = myurl.replace(" ","%20");
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("respuesta", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
     }
 }
--- SGG/app/src/main/java/com/example/eisra/sgg/MainActivity.java
+++ SGG/app/src/main/java/com/example/eisra/sgg/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.eisra.sgg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registrar = (TextView) findViewById(R.id.textView13);
+    }
+
+    public void abrirRegistro(View v)
+    {
+        Intent i= new Intent(this, Registro.class);
+        startActivity(i);
     }
 }
