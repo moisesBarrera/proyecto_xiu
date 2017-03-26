dc676eeb0effbe1af3cca880145a2f38feaea945
25ls <ernesto25ls@gmail.com>
2017-03-26 09:40:02 -0500

    Proyecto android
    Es lo que llevo jeje

---
+++ SGG/app/src/main/java/com/example/eisra/sgg/TareasDeEquipos.java
@@ -0,0 +1,13 @@
+package com.example.eisra.sgg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TareasDeEquipos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tareas_de_equipos);
+    }
+}
